fc3560ee2098928655b6fcc4c75fd4630680e909
newniki <112488452+newniki@users.noreply.github.com>
2022-08-30 20:39:36 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -61,7 +61,7 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-Welcome | Dhruv Mirror Bot is ready for you
+Welcome | Mirror Bot is ready for you
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
