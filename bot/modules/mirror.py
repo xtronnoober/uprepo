795f63238919e40e1c421e158dd7421634bbe685
xtronnoober <85035009+xtronnoober@users.noreply.github.com>
2023-02-05 19:36:38 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -539,7 +539,8 @@ def _mirror(bot, message, isZip=False, extract=False, isQbit=False, isLeech=Fals
             auth = f"{ussr}:{pssw}"
             auth = "Basic " + b64encode(auth.encode()).decode('ascii')
         else:
-            sendMessage('Lamao Ded', bot, message)
+            Thread(target=_mirror, args=(bot, nextmsg, isZip, extract, isQbit, isLeech, pswd, multi)).start()
+#            sendMessage('Lamao Ded', bot, message)
 #            auth = ''
 #        Thread(target=add_aria2c_download, args=(link, f'{DOWNLOAD_DIR}{listener.uid}', listener, name, auth)).start()
 
